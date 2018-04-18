891966a61390a4f80d90893dd833cbe041fbb5c2
Swork91 <chicagomike999@gmail.com>
2018-04-18 15:33:38 -0400

    execution

--- src/TaskThreadDemo.java
+++ src/TaskThreadDemo.java
@@ -1,11 +1,30 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import javax.swing.*;
 
 public class TaskThreadDemo {
 
 	public static void main(String[] args) {
-		Runnable printA = new PrintChar('a', 100);
-		Runnable printB = new PrintChar('b', 100);
-		Runnable print100 = new PrintNum(100);
+		//using threads instead of timers
+		FlashingText donk = new FlashingText();
+		JFrame frame = new JFrame();
+		frame.add(donk);
+		frame.setSize(400, 300);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+		//threads
+		//createThreads();
+		
+		//executors
+		createExecutors(3);
+	}
+	
+	public static void createThreads() {
+		Runnable printA = new PrintChar('a', 1000);
+		Runnable printB = new PrintChar('b', 1000);
+		Runnable print100 = new PrintNum(1000);
 		
 		//creating threads
 		Thread thread1 = new Thread(printA);
@@ -15,13 +34,19 @@ public class TaskThreadDemo {
 		thread1.start();
 		thread2.start();
 		thread3.start();
-		
-		FlashingText donk = new FlashingText();
-		JFrame frame = new JFrame();
-		frame.setVisible(true);
-		frame.add(donk);
 	}
-
+	
+	public static void createExecutors(int t) {
+		int threadsToUse = t;
+		//create thread pool of max t
+		ExecutorService executor =  Executors.newFixedThreadPool(threadsToUse);
+		
+		executor.execute(new PrintChar('a', 1000));
+		executor.execute(new PrintChar('b', 1000));
+		executor.execute(new PrintNum(1000));
+		
+		executor.shutdown();
+	}
 }
 
 class PrintChar implements Runnable {
